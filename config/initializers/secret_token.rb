6a3f2a4923eded9ffd15ae53f264f7613365979d
Yihuan Zhou <huan@oneblueham.com>
2012-04-28 19:01:25 -0400

    initial scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CMUPdp::Application.config.secret_token = 'df7d6fcf2e3ca3a6287b72f195f1b78d56823a99832c6a39307771d697b557c581d57841155339a5799c03c166bed8d655ce548216371c168580f8596aa48689'
